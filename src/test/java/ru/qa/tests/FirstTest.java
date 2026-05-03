c43f18bce7f3c55eba0b36f6c1660a09f6f1686f
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-05-03 12:59:27 +0300

    refactor first test

--- src/test/java/ru/qa/tests/FirstTest.java
+++ src/test/java/ru/qa/tests/FirstTest.java
@@ -1,14 +1,26 @@
 package ru.qa.tests;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FirstTest {
     @Test
-    void openGoogleTest() {
+    void openGoogleTest() throws InterruptedException {
         WebDriver driver = new ChromeDriver(); // запуск Chrome
         driver.get("https://www.google.com/"); // открытие страницы
+        assertTrue(driver.getTitle().contains("Google")); // проверка нахождения на странице гугла
+        WebElement searchForm = driver.findElement(By.cssSelector(".gLFyf[title=\"Поиск\"]")); // ищем форму ввода
+        //  xpath вариант:
+        //  WebElement searchForm = driver.findElement(By.xpath("//*[@title=\"Поиск\"]"));
+        searchForm.sendKeys("Прогноз погоды"); // вводим значение
+        searchForm.sendKeys(Keys.ENTER); // нажимаем enter
+        Thread.sleep(3000); // физическое ожидание теста 3 сек
         driver.quit(); // закрытие браузера
     }
 }
